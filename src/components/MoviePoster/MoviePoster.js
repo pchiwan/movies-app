98916b973f6345ffd0b73311b0f0b566b5395545
Sílvia Mur Blanch <pchiwan@gmail.com>
2021-09-04 13:26:26 +0200

    Add fav controls to MoviePoster component

--- src/components/MoviePoster/MoviePoster.js
+++ src/components/MoviePoster/MoviePoster.js
@@ -1,12 +1,70 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 import "./MoviePoster.scss";
 
-function MoviePoster({ src }) {
+const HOLLOW_STAR = "far fa-star";
+const SOLID_STAR = "fas fa-star";
+
+function MoviePoster({ isFavControlVisible, isFavorited, src }) {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+
   const style = {
     backgroundImage: `url(${src})`,
   };
-  return <div className="movie-poster" style={style} />;
+
+  const handleMouseDown = useCallback(() => {
+    setIsActive(true);
+  }, []);
+
+  const handleMouseUp = useCallback(() => {
+    setIsActive(false);
+  }, []);
+
+  const handleMouseEnter = useCallback(() => {
+    setIsHovered(true);
+  }, []);
+
+  const handleMouseOut = useCallback(() => {
+    setIsHovered(false);
+  }, []);
+
+  let starClassName = isActive
+    ? isFavorited
+      ? HOLLOW_STAR
+      : SOLID_STAR
+    : isFavorited
+    ? SOLID_STAR
+    : HOLLOW_STAR;
+
+  return (
+    <div
+      className={`movie-poster-wrapper ${
+        isFavControlVisible ? "clickable" : ""
+      }`}
+    >
+      {isFavControlVisible && (
+        <div
+          className="movie-poster-fav-control ripple"
+          onMouseEnter={handleMouseEnter}
+          onMouseOut={handleMouseOut}
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+        >
+          <i
+            className={starClassName}
+            style={{ display: isHovered ? "block" : "none" }}
+          />
+        </div>
+      )}
+      {isFavorited && (
+        <div className="movie-poster-fav">
+          <i className={SOLID_STAR} />
+        </div>
+      )}
+      <div className="movie-poster" style={style} />
+    </div>
+  );
 }
 
 export default MoviePoster;
